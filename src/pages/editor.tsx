430ced70c3e46473f3c266dd304f9ca066913127
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-03 01:41:24 +0900

    feat: dashboard & editor 페이지 생성

---
+++ src/pages/editor.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import React from "react";
+
+const Editor = () => {
+  return (
+    <>
+      <Head>
+        <title>Editor</title>
+        <meta
+          name="description"
+          content="Automate Archisketch Process for Editor Team"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      Editor
+    </>
+  );
+};
+
+export default Editor;
