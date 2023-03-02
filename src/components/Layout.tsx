3affe54ce8732185ad857cc568087e8ecfc51b37
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-03 01:42:30 +0900

    style: layout main 스타일 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -107,7 +107,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       </header>
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">{children}</div>
+          <div className="px-4 sm:px-0">{children}</div>
         </div>
       </main>
     </div>
