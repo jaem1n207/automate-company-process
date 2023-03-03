42f8efc3bef66f00ecf18bf93fb78330139e2563
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-03 18:23:25 +0900

    chore: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-const pxToRem = (/** @type {number} */ px) => `${px / 16}rem`;
-
-const range = (/** @type {number} */ start, /** @type {number} */ end) => {
-  const length = end - start + 1;
-  return Array.from({ length }, (_, i) => start + i);
-};
-
 /** @type {import('tailwindcss').Config} */
 const config = {
   future: {
@@ -17,34 +8,6 @@ const config = {
   theme: {
     data: {
       active: 'ui~="active"',
-    },
-    fontSize: {
-      ...range(0, 100).reduce((acc, px) => {
-        // @ts-ignore
-        acc[`${px}pxr`] = pxToRem(px);
-        return acc;
-      }, {}),
-    },
-    spacing: {
-      ...range(0, 100).reduce((acc, px) => {
-        // @ts-ignore
-        acc[`${px}pxr`] = pxToRem(px);
-        return acc;
-      }, {}),
-    },
-    borderWidth: {
-      ...range(0, 10).reduce((acc, px) => {
-        // @ts-ignore
-        acc[`${px}pxr`] = pxToRem(px);
-        return acc;
-      }, {}),
-    },
-    screens: {
-      display: { max: "1440px" },
-      desktop: { max: "1024px" },
-      tablet: { max: "768px" },
-      foldable: { max: "523px" },
-      mobile: { max: "360px" },
     },
     extend: {},
   },
