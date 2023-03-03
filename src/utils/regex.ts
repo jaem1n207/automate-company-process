e2c966904615710447f096cb81c42192e27ea737
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-03 18:30:41 +0900

    feat: regex util

---
+++ src/utils/regex.ts
@@ -0,0 +1,6 @@
+export const regex = {
+  kebakCase: new RegExp("^[a-z]+(?:-[a-z]+)*$"),
+  email: new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+  password: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"),
+  url: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"),
+};
