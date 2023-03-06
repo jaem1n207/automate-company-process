b0dcde6264bddbee6bb3b5c3698eea4361b2b058
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-06 15:02:36 +0900

    :sparkles: SavePathModal 작성

---
+++ src/components/SavePathModal.tsx
@@ -0,0 +1,177 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import { regex } from "@/utils/regex";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+type Props = {
+  initialValue: string;
+  visible: boolean;
+  onClose: () => void;
+  onSave: (value: string) => void;
+};
+
+const SavePathModal = ({ initialValue, visible, onClose, onSave }: Props) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleSave = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (value.trim() === "") {
+      setError("값을 입력해주세요.");
+    } else {
+      onSave(value);
+      onClose();
+    }
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value.trim();
+    setValue(val);
+    if (val === "") {
+      setError("");
+    } else if (regex.directoryPath.test(val) === false || val.includes(" ")) {
+      setError("유효한 디렉토리 경로를 입력해주세요.");
+    } else {
+      setError("");
+    }
+  };
+
+  const errorVariants = {
+    hidden: {
+      opacity: 0,
+      height: 0,
+    },
+    visible: {
+      opacity: 1,
+      height: "auto",
+    },
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        event.stopPropagation();
+      }
+    };
+
+    if (!initialValue) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      if (!initialValue) {
+        document.removeEventListener("keydown", handleKeyDown);
+      }
+    };
+  }, [onClose, initialValue]);
+
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return (
+    <Transition appear show={visible} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={
+          initialValue
+            ? onClose
+            : () => {
+                return;
+              }
+        }
+        ref={modalRef}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <span className="inline-block h-screen align-middle" aria-hidden="true">
+          &#8203;
+        </span>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                {initialValue && (
+                  <button
+                    className="absolute top-0 right-0 p-2 text-gray-500 hover:text-gray-600"
+                    onClick={onClose}
+                  >
+                    <XMarkIcon className="h-5 w-5" />
+                  </button>
+                )}
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Set Language Path
+                </Dialog.Title>
+                <form className="space-y-4" onSubmit={handleSave}>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      value={value}
+                      onChange={handleInput}
+                      className={`border ${
+                        error ? "border-red-500" : "border-gray-300"
+                      } w-full rounded-md px-3 py-2`}
+                      placeholder="C:\\example\\path"
+                    />
+
+                    <AnimatePresence>
+                      {error && (
+                        <motion.div
+                          className="mt-1 text-sm text-red-500"
+                          variants={errorVariants}
+                          initial="hidden"
+                          animate="visible"
+                          exit="hidden"
+                        >
+                          {error}
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default SavePathModal;
