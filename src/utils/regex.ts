75e19eaf9dae38e515cc825ae73c883d6fe01392
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-06 13:31:29 +0900

    feat: directory path regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,4 +3,5 @@ export const regex = {
   email: new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
   password: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"),
   url: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"),
+  directoryPath: new RegExp("^(.+)/([^/]+)$"),
 };
