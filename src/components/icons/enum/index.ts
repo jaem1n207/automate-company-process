94d1ff899ffe76b5f54e73f307c39f7486d65eb4
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-10 13:34:32 +0900

    :sparkles: routes 모듈 레벨로 관리

---
+++ src/components/icons/enum/index.ts
@@ -0,0 +1,13 @@
+interface IconSize {
+  small: number;
+  medium: number;
+  large: number;
+  xlarge: number;
+}
+
+export const IconSize: IconSize = {
+  small: 16,
+  medium: 24,
+  large: 32,
+  xlarge: 64,
+};
---
+++ src/enum/index.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  DASHBOARD: "/",
+  EDITOR: "/editor",
+  TEAM_SELECTION: "/teamSelection",
+};
+
+export const LOCAL_STORAGE_KEYS = {
+  MY_TEAM: "my-team",
+  LANG_PATH: "langPath",
+} as const;
