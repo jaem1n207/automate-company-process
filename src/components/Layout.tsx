63619c7b013fc7fe81944a80aec696545bba979d
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-12 22:15:19 +0900

    🐛 fix: layout 조직 예외처리

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -144,7 +144,12 @@ const Layout: React.FC<LayoutProps> = ({ children, path: pathProp }) => {
       <header className="bg-white">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-4xl font-bold text-gray-900">
-            조직: {router.pathname === "/" ? "Dashboard" : "Editor"}
+            조직:{" "}
+            {router.pathname === "/"
+              ? "Dashboard"
+              : router.pathname === "/editor"
+              ? "Editor"
+              : "Unknown"}
           </h1>
         </div>
       </header>
